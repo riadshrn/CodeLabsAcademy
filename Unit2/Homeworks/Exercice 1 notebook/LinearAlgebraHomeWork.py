db707d14f4d8a851a1d670bb036100c6e85e6e72
your_username <km_sahrane@esi.dz>
2021-12-12 19:55:29 +0100

    Done

---
+++ Unit2/Homeworks/Exercice 1 notebook/LinearAlgebraHomeWork.py
@@ -0,0 +1,46 @@
+import numpy as np
+from numpy.linalg import norm
+from os import system,name
+shape = int(input("-->Please Enter the shape of your matrix: "))
+matrix = np.zeros((shape,shape))
+
+for i in range (shape):
+        for j in range (shape):
+            matrix[i,j] = int(input("T[%d,%d]= "%(i,j)))
+        print("\n")
+
+system('cls')
+
+print ("The matrix is : \n",matrix)
+
+def LowerTriangular(matrix,shape):
+    for i in range (shape):
+        for j in range (i+1,shape):
+            if(matrix[i,i])==0 or matrix[i,j]!=0:
+                return False
+    return True
+
+
+def UpperTriangular(matrix,shape):
+    return LowerTriangular(matrix.T,shape)
+
+def DiagonalMatrix(matrix,shape):
+    return (UpperTriangular(matrix,shape) and LowerTriangular(matrix,shape))
+
+if (LowerTriangular(matrix,shape)): print("\n\t\t[The matrix is a Lower Triangular Matrix ]")
+else: print("\n\t\t[The matrix is not a Lower Triangular Matrix ]")  
+
+if (UpperTriangular(matrix,shape)): print("\n\t\t[The matrix is a Upper Triangular Matrix ]")
+else: print("\n\t\t[The matrix is not a Upper Triangular Matrix ]")  
+
+
+if (DiagonalMatrix(matrix,shape)): print("\n\t\t[The matrix is a Diagonal Matrix ]")
+else: print("\n\t\t[The matrix is not a Diagonal Matrix ]")  
+
+
+column = int(input("-->Please choice a column from 1 to %d  : "%(shape)))
+print("\tThe column chocen is",matrix.T[column-1])
+
+
+print("-->The norm 1 of the vector is : %d"%(norm(matrix.T[column-1],1)))
+print("-->The norm 2 of the vector is : %f"%(norm(matrix.T[column-1])))
