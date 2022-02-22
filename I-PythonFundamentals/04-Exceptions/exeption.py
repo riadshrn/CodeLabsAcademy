f4ddbfa3e376007d9df1e61514f534eed70eb43b
riadshrn <km_sahrane@esi.dz>
2022-02-22 20:14:50 +0100

    exeption modified

--- I-PythonFundamentals/04-Exceptions/exeption.py
+++ I-PythonFundamentals/04-Exceptions/exeption.py
@@ -1,5 +1,18 @@
 a = 12
-s = "hello"
-print("inside try")
-print(a + s) # will raise TypeError
-print("Printed using original data types")
+s = "Hello"
+try:
+	print("inside try")
+	print(a + s) # will raise TypeError
+	print("Printed using original data types")
+except TypeError: # will handle only TypeError
+	print("inside except")
+	print(str(a) + s)
+	print("Printed using type-casted data types")
+
+except NameError as N_err:
+	print(N_err)
+	print("NameError occurred. Some variable isn't defined.")
+
+except MemoryError as M_err :
+	print(M_err)
+	print("MemoryError occurred. Some variable are big.")
